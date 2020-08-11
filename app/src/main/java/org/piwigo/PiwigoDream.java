cf450ccef55ec519238258367444b622d834b491
Per Sandström <per.j.sandstrom@gmail.com>
2020-08-11 14:44:13 +0200

    dream stub

---
+++ app/src/main/java/org/piwigo/PiwigoDream.java
@@ -0,0 +1,10 @@
+package org.piwigo;
+
+import android.service.dreams.DreamService;
+
+public class PiwigoDaydream extends DreamService {
+    @Override
+    public void onDreamingStarted() {
+        super.onDreamingStarted();
+    }
+}
